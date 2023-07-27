45680a8dfdd84d5574d3b838af45b28f7409f5bb
arafatdayan005 <arafatdayan005@gmail.com>
2023-07-27 22:41:32 +0600

    react router setup added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
+import { Outlet } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
     <>
-      <h1 className="text-3xl text-red-500 underline">
-        Hello world!
-      </h1>
+      <Outlet></Outlet>
     </>
   );
 }
