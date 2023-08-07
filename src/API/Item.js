60a73122d996ac954e9efa3176eb7029cd61b167
arafatdayan005 <arafatdayan005@gmail.com>
2023-08-08 03:57:15 +0600

    Additem Page Added

---
+++ src/API/Item.js
@@ -0,0 +1,11 @@
+export const postItem = (data) => {
+    fetch('http://localhost:5000/items', {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+        .then(res => res.json())
+        .then(d => console.log(d))
+}
